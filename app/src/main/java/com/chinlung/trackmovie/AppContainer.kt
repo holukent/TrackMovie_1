f8eec1d5c473a409b695392b36b5004e6d6504e8
PAN <pan910421@gmail.com>
2021-10-07 21:34:46 +0800

    test

--- app/src/main/java/com/chinlung/trackmovie/AppContainer.kt
+++ app/src/main/java/com/chinlung/trackmovie/AppContainer.kt
@@ -2,6 +2,7 @@ package com.chinlung.trackmovie
 
 import com.chinlung.trackmovie.room.roomdatabase.TmdbDataBase
 
+
 class AppContainer(private val myApplication: MyApplication) {
     val db:TmdbDataBase by lazy {
         TmdbDataBase.getDatabase(myApplication)
